54ad8a2942d1aaa70d20d3e5665ff40130ad93d6
minju <minjuuuz@gmail.com>
2023-01-22 18:56:31 +0900

    Initial commit

---
+++ src/main/java/com/maven/mmsProj/MmsProjApplication.java
@@ -0,0 +1,13 @@
+package com.maven.mmsProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/mmsProj/MmsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.mmsProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
